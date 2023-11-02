c71e9a447679dd816d19c7187f0b1178e3149be6
Jean Carlos <jeancarlosaps@yahoo.com.br>
2023-11-02 19:49:43 -0300

    Feature: Configuração inicial do projeto para utilizar viewCode.

---
+++ DigioChallengeiOS/DigioChallengeiOS/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  DigioChallengeiOS
+//
+//  Created by Jean Carlos on 02/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
